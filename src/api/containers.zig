6137fc0c8587ad2fba03aeb7c80d5c49f4cf454d
Ermal Guni <ermalguni@gmail.com>
2026-09-15 17:59:54 +0200

    Container create and url query params generalization

--- src/api/containers.zig
+++ src/api/containers.zig
@@ -1,10 +1,41 @@
 const std = @import("std");
 const Client = @import("../client.zig").Client;
 const models = @import("../generated/models.zig");
+const QueryParam = @import("../http.zig").QueryParam;
+const query_param_to_url = @import("../http.zig").query_params_tu_url_encoded_string;
 
 pub const ListOptions = struct {
     all: ?bool = null,
     limit: ?u32 = null,
+};
+
+pub const ContainerCreateRequest = struct {
+    config: models.ContainerConfig,
+    host_config: ?models.HostConfig,
+    networking_config: ?models.NetworkingConfig,
+
+    pub fn toJsonString(self: ContainerCreateRequest, json_string: *std.json.Stringify) !void {
+        try json_string.beginObject();
+
+        for (std.meta.fields(models.ContainerConfig)) |field| {
+            if (@field(self.config, field.name)) |value| {
+                try json_string.objectField(field.name);
+                try json_string.write(value);
+            }
+        }
+
+        if (self.host_config) |host| {
+            try json_string.objectField("HostConfig");
+            try json_string.write(host);
+        }
+
+        if (self.networking_config) |network| {
+            try json_string.objectField("NetworkingConfig");
+            try json_string.write(network);
+        }
+
+        try json_string.endObject();
+    }
 };
 
 // API endpoint paths
@@ -59,6 +90,21 @@ pub const Containers = struct {
             .target = writer.buffered(),
             .versioned = true,
         });
+    }
+
+    pub fn create(self: Containers, request: ContainerCreateRequest, params: []const QueryParam) !std.json.Parsed(models.ContainerCreateResponse) {
+        var target: std.Io.Writer.Allocating = .init(self.client.allocator);
+        defer target.deinit();
+
+        const writer = &target.writer;
+
+        query_param_to_url(writer, params);
+
+        const payload = try std.json.Stringify.valueAlloc(self.client.allocator, request, .{
+            .emit_null_optional_fields = false,
+        });
+
+        return self.client.getJson(models.ContainerCreateResponse, .{ .target = writer.buffered(), .versioned = true, .method = .POST, .expected_status = .created, .payload = payload, .content_type = "application/json" });
     }
 };
 
---
+++ src/http.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub const QueryValue = union(enum) {
+    string: []const u8,
+    int: i64,
+    uint: u64,
+};
+
+pub const QueryParam = struct { name: []const u8, value: QueryValue };
+
+pub fn query_params_tu_url_encoded_string(writer: *std.Io.Writer, params: []const QueryParam) !void {
+    if (params.len == 0) return;
+
+    try writer.writeByte('?');
+
+    for (params, 0..) |param, i| {
+        if (i == 0) try writer.writeByte("&");
+
+        const key: std.Uri.Component = .{ .raw = param.name };
+        try key.formatEscaped(writer);
+        try writer.writeByte('=');
+
+        switch (param.value) {
+            .int => |value| try writer.print("{d}", value),
+            .uint => |value| try writer.print("{d}", value),
+            .string => |value| {
+                const component: std.Uri.Component = .{ .raw = value };
+                try component.formatEscaped(writer);
+            },
+        }
+    }
+}
